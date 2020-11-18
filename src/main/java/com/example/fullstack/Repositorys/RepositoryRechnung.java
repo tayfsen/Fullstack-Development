8d0a766d5056e556f6341c51f1f56834e6db58b0
Tayfun Sen <sen.ghost@outlook.com>
2020-11-18 14:45:12 +0100

    Habe den Service Layer und die Repositorys bearbeitet

--- src/main/java/com/example/fullstack/Repositorys/RepositoryRechnung.java
+++ src/main/java/com/example/fullstack/Repositorys/RepositoryRechnung.java
@@ -3,6 +3,25 @@ package com.example.fullstack.Repositorys;
 import com.example.fullstack.Model.Rechnung;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 public interface RepositoryRechnung extends JpaRepository<Rechnung, Long> {
+
+    int insertRechnung(UUID id, Rechnung rechnung);
+
+    default int insertRechnung(Rechnung rechnung)
+
+    {
+        UUID id = UUID.randomUUID();
+
+        return insertRechnung(id, rechnung);
+    }
+
+    List<Rechnung> selectAllRechnung();
+    Optional<Rechnung> selectRechnungById();
+
+    int deleteById(UUID id);
 
 }
---
+++ src/main/java/com/example/fullstack/Service/RechnungService.java
@@ -0,0 +1,44 @@
+package com.example.fullstack.Service;
+
+
+import com.example.fullstack.Model.Rechnung;
+import com.example.fullstack.Repositorys.RepositoryRechnung;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class RechnungService {
+    private RepositoryRechnung repositoryRechnung;
+
+
+    @Autowired
+    public RechnungService(RepositoryRechnung repositoryRechnung)
+    {
+        this.repositoryRechnung = repositoryRechnung;
+    }
+
+    public int addRechnung(Rechnung rechnung)
+    {
+        return repositoryRechnung.insertRechnung(rechnung);
+    }
+
+    public List<Rechnung> getAllRechnung()
+    {
+        return repositoryRechnung.selectAllRechnung();
+    }
+
+    public Optional<Rechnung> getRechnungById(UUID id)
+    {
+        return repositoryRechnung.selectRechnungById();
+    }
+
+    public int deleteRechnung(UUID id)
+    {
+        return repositoryRechnung.deleteById(id);
+    }
+
+}
