715b9fb13c2e66c2017d42de4ef58027cb53ccb0
Tayfun Sen <sen.ghost@outlook.com>
2020-11-18 14:01:14 +0100

    Habe die Entitaet Kunde hinzugefuegt

---
+++ src/main/java/com/example/fullstack/Model/Kunde.java
@@ -0,0 +1,52 @@
+package com.example.fullstack.Model;
+
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.util.List;
+
+@Entity
+public class Kunde {
+
+    private String id;
+    private String Nachname;
+    private String Vorname;
+    private List<Rechnung> rechnungen;
+
+    @Id
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNachname() {
+        return Nachname;
+    }
+
+    public void setNachname(String nachname) {
+        Nachname = nachname;
+    }
+
+    public String getVorname() {
+        return Vorname;
+    }
+
+    public void setVorname(String vorname) {
+        Vorname = vorname;
+    }
+
+    public List<Rechnung> getRechnungen() {
+        return rechnungen;
+    }
+
+    public void setRechnungen(List<Rechnung> rechnungen) {
+        this.rechnungen = rechnungen;
+    }
+
+
+
+
+}
