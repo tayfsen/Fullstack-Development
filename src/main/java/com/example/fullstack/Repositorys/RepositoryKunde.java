543ce0ecb991b3375463e572e6e96900b44337de
Tayfun Sen <sen.ghost@outlook.com>
2020-11-18 14:27:40 +0100

    Habe die application.properties hinzugefügt und die Repositorys bearbeitet

--- src/main/java/com/example/fullstack/Repositorys/RepositoryKunde.java
+++ src/main/java/com/example/fullstack/Repositorys/RepositoryKunde.java
@@ -3,5 +3,22 @@ package com.example.fullstack.Repositorys;
 import com.example.fullstack.Model.Kunde;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 public interface RepositoryKunde extends JpaRepository<Kunde,Long> {
+    int insertKunde(UUID id, Kunde kunde);
+    default int insertKunde(Kunde kunde)
+    {
+    UUID id =UUID.randomUUID();
+    return insertKunde(id, kunde);
+    }
+
+    List<Kunde> selectAllKunde();
+    Optional<Kunde> selectKundeById();
+
+    int deleteById(UUID id);
+
+
 }
---
+++ src/main/java/com/example/fullstack/Service/KundenService.java
@@ -0,0 +1,43 @@
+package com.example.fullstack.Service;
+
+import com.example.fullstack.Model.Kunde;
+import com.example.fullstack.Repositorys.RepositoryKunde;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class KundenService {
+
+    private RepositoryKunde repositoryKunde;
+
+    @Autowired
+    public KundenService(RepositoryKunde repositoryKunde)
+    {
+    this.repositoryKunde = repositoryKunde;
+    }
+
+    public int addKunde(Kunde kunde)
+    {
+    return repositoryKunde.insertKunde(kunde);
+    }
+
+    public List<Kunde> getAllKunde()
+    {
+    return repositoryKunde.selectAllKunde();
+    }
+
+    public Optional<Kunde> getKundeById(UUID id)
+    {
+        return repositoryKunde.selectKundeById();
+    }
+
+    public int deleteKunde (UUID id)
+    {
+        return repositoryKunde.deleteById(id);
+    }
+
+}
